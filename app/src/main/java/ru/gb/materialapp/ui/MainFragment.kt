592d1fec74bb47ee90172627007b567cf9528bdf
Pavelbour <pavelbour@gmail.com>
2022-04-30 22:59:02 +0300

    Image loading.

---
+++ app/src/main/java/ru/gb/materialapp/ui/MainFragment.kt
@@ -0,0 +1,53 @@
+package ru.gb.materialapp.ui
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.coroutineScope
+import coil.load
+import kotlinx.coroutines.flow.collect
+import ru.gb.materialapp.R
+import ru.gb.materialapp.databinding.FragmentMainBinding
+import ru.gb.materialapp.domain.NasaRepositoryImpl
+
+class MainFragment: Fragment(R.layout.fragment_main) {
+    private val viewModel: MainViewModel by viewModels {
+        MainViewModelFactory(NasaRepositoryImpl())
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            viewModel.requestPictureOfTheDay()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentMainBinding.bind(view)
+
+        viewLifecycleOwner.lifecycle.coroutineScope.launchWhenStarted {
+            viewModel.loading.collect {
+                binding.mainFragmentProgress.visibility = if (it) View.VISIBLE else View.GONE
+            }
+        }
+
+        viewLifecycleOwner.lifecycle.coroutineScope.launchWhenStarted {
+            viewModel.error.collect {
+                Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        viewLifecycleOwner.lifecycle.coroutineScope.launchWhenStarted {
+            viewModel.image.collect { url ->
+                url?.let {
+                    binding.mainFragmentImage.load(it)
+                }
+            }
+        }
+    }
+}
